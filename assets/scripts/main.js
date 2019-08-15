cf27086c3dab83b09010562fb5a47b53b553fdd9
Carl Janz <carl.janz@gmail.com>
2019-08-14 22:56:31 -0700

    Updated blog with new deck changes. Also moved preview card to top right of the screen and allowed the user to click on it to dismiss it.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,35 +1,49 @@
 const localStorage = window.localStorage;
 let cardImage;
 
-function getCardInfo(name) {
+function getCardInfo(name, set) {
   let cardInfo = readFromLocal(name); // Check to see if card info is in local storage
   if (cardInfo) {
     console.log("Getting card from local storage");
     drawCard(cardInfo); // If so use data from there.
   } else {
     // If not, hit the API and get the card info.
-    console.log("Hitting API.");
+    console.log("Card not in local storage, Hitting API.");
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         let cardList = JSON.parse(this.responseText).cards;
-        // console.log(cardList);
-        let i = 0;
-        while (cardInfo === false && i < cardList.length) {
-          if (Object.keys(cardList[i]).includes("imageUrl")) {
-            // Some listings don't have imageUrls. This looks for a listing that does and uses that if possible.
-            console.log(i, "Image url found");
-            cardInfo = cardList[i];
+        console.log(cardList);
+        if (cardInfo === false) {
+          if (set) {
+            console.log("Set passed, finding specified set");
+            for (var i = 0; i < cardList.length; i++) {
+              if (set === cardList[i].set) {
+                cardInfo = cardList[i];
+              }
+            }
+            if (!cardInfo) {
+            }
           } else {
-            console.log(i, "Image url not found");
-            i++;
+            let i = 0;
+            while (i < cardList.length) {
+              if (Object.keys(cardList[i]).includes("imageUrl")) {
+                // Some listings don't have imageUrls. This looks for a listing that does and uses that if possible.
+                console.log(i, "Image url found");
+                cardInfo = cardList[i];
+                i = cardList.length;
+              } else {
+                console.log(i, "Image url not found");
+                i++;
+              }
+            }
           }
         }
         console.log(cardInfo);
         if (!cardInfo) {
           cardInfo = cardList[0];
         }
-        sendToLocal(cardInfo);
+        sendToLocalStorage(cardInfo);
         drawCard(cardInfo);
       }
     };
@@ -43,20 +57,27 @@ function getCardInfo(name) {
 }
 
 window.onload = () => {
-  console.log("loaded");
   cardList = document.getElementsByClassName("card");
   for (let i = 0; i < cardList.length; i++) {
-    console.log(cardList[i]);
     cardList[i].onclick = function() {
-      console.log(this.attributes["data-name"].value);
-      getCardInfo(this.attributes["data-name"].value);
+      let name = this.attributes["data-name"].value;
+      let set;
+      if ("data-set" in this.attributes) {
+        set = this.attributes["data-set"].value;
+      }
+      console.log("Name:", name, "Set:", set);
+      getCardInfo(name, set);
     };
   }
   cardImage = document.getElementById("image-div");
+  cardImage.onclick = function() {
+    let emptyDiv = document.createElement("div");
+    this.replaceChild(emptyDiv, cardImage.firstChild);
+  };
 };
 
 // Send data for a card to local storage to save on API hits
-function sendToLocal(card) {
+function sendToLocalStorage(card) {
   localStorage.setItem(card.name.toLowerCase(), JSON.stringify(card));
 }
 
